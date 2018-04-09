256ee3e85bd8579111a5c6e2b494f0557f66c986
ocramz <zocca.marco@gmail.com>
2018-04-09 05:07:55 +0200

    don't import Real

--- numhask/src/NumHask/Algebra/Ring.hs
+++ numhask/src/NumHask/Algebra/Ring.hs
@@ -15,7 +15,7 @@ import Data.Complex (Complex(..))
 import NumHask.Algebra.Additive
 import NumHask.Algebra.Distribution
 import NumHask.Algebra.Multiplicative
-import Prelude (Bool(..), Double, Float, Int, Integer, Real(..))
+import Prelude (Bool(..), Double, Float, Int, Integer)
 
 -- | Semiring
 class (MultiplicativeAssociative a, MultiplicativeUnital a, Distribution a) =>
