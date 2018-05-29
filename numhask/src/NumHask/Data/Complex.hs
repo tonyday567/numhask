57d46016a2c36bac6e648bcc06f2c12803155907
Marco Zocca <marco.zocca@recordunion.com>
2018-05-29 13:25:19 +0200

    add some instances

--- numhask/src/NumHask/Data/Complex.hs
+++ numhask/src/NumHask/Data/Complex.hs
@@ -10,7 +10,8 @@ import NumHask.Algebra.Ring
 import NumHask.Algebra.Distribution
 import NumHask.Algebra.Field
 
-import Prelude hiding (Num(..), negate, sin, cos, sqrt)
+import Prelude hiding (Num(..), negate, sin, cos, sqrt, (/), atan, pi, exp, log, recip)
+import qualified Prelude as P ( (&&), (>), (<=), (<), (==), otherwise, Ord(..) )
 
 -- -----------------------------------------------------------------------------
 -- The Complex type
@@ -69,6 +70,20 @@ instance (MultiplicativeMagma a, AdditiveGroup a) => MultiplicativeMagma (Comple
   (rx :+ ix) `times` (ry :+ iy) =
     (rx `times` ry - ix `times` iy) :+ (ix `times` ry + iy `times` rx)
 
+instance (MultiplicativeMagma a, AdditiveGroup a) => MultiplicativeCommutative (Complex a)
+
+instance (MultiplicativeUnital a, MultiplicativeAssociative a, AdditiveGroup a) => Multiplicative (Complex a)
+
+
+instance (AdditiveGroup a, MultiplicativeInvertible a) => MultiplicativeInvertible (Complex a) where
+  recip (rx :+ ix) = (rx `times` d) :+ (negate ix `times` d)
+    where
+      d = recip ((rx `times` rx) `plus` (ix `times` ix))
+
+
+
+instance (MultiplicativeUnital a, MultiplicativeAssociative a, MultiplicativeInvertible a, AdditiveGroup a) => MultiplicativeGroup (Complex a)    
+
 
 
 
@@ -78,7 +93,31 @@ instance (AdditiveGroup a, MultiplicativeAssociative a) =>
 
 instance (Semiring a, AdditiveGroup a) => Semiring (Complex a)
 
+instance (Semiring a, AdditiveGroup a) => Ring (Complex a)
+
 instance (Semiring a, AdditiveGroup a) => InvolutiveRing (Complex a)
+
+
+
+instance (MultiplicativeGroup a, AdditiveGroup a, Semiring a) => Field (Complex a) 
+
+
+
+
+-- | todo: bottom is here somewhere???
+instance (P.Ord a, TrigField a, ExpField a) => ExpField (Complex a) where
+  exp (rx :+ ix) = exp rx * cos ix :+ exp rx * sin ix
+  log (rx :+ ix) = log (sqrt (rx * rx + ix * ix)) :+ atan2 ix rx
+    where
+      atan2 y x
+        | x P.> zero = atan (y / x)
+        | x P.== zero P.&& y P.> zero = pi / (one + one)
+        | x P.< one P.&& y P.> one = pi + atan (y / x)
+        | (x P.<= zero P.&& y P.< zero) || (x P.< zero) =
+          negate (atan2 (negate y) x)
+        | y P.== zero = pi -- must be after the previous test on zero y
+        | x P.== zero P.&& y P.== zero = y -- must be after the other double zero tests
+        | P.otherwise = x + y -- x or y is a NaN, return a NaN (via +)
 
 
 
