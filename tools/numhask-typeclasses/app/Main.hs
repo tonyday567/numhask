295c245f980193a590ebab3971c82c5df72ca203
Marco Zocca <marco.zocca@recordunion.com>
2018-05-31 19:19:58 +0200

    add some haskell-src-exts helpers, FIXME partial matches

--- tools/numhask-typeclasses/app/Main.hs
+++ tools/numhask-typeclasses/app/Main.hs
@@ -37,6 +37,54 @@ unpackTyClDecl decl = case decl of
 --   ClsDecl _ ()
 
 
+
+
+uContext :: Context l -> Context ()
+uContext c = case c of
+  CxSingle _ a -> CxSingle () $ uAsst a
+
+uAsst :: Asst l -> Asst ()
+uAsst a = case a of
+  ClassA _ qn tys -> ClassA () (uQName qn) (map uType tys)
+  AppA _ n tys -> AppA () (uName n) (map uType tys)
+  InfixA _ ty0 qn ty -> InfixA () (uType ty0) (uQName qn) (uType ty)
+  IParam _ ipn ty -> IParam () (uIPName ipn) (uType ty)
+  EqualP _ ty ty1 -> EqualP () (uType ty) (uType ty1)
+  ParenA _ asst -> ParenA () (uAsst asst)
+  -- WildCardA _ mn ->
+
+uIPName :: IPName l -> IPName ()
+uIPName i = case i of
+  IPDup _ s -> IPDup () s
+  IPLin _ s -> IPLin () s
+
+uType :: Type l -> Type ()
+uType t = case t of
+  -- TyForall _
+  TyFun _ ty ty1 -> TyFun () (uType ty) (uType ty1)
+  TyTuple _ b tys -> TyTuple () b (map uType tys)
+
+uQName :: QName l -> QName ()
+uQName qn = case qn of
+  Qual _ mn n -> Qual () (uModuleName mn) (uName n)
+  UnQual _ n -> UnQual () (uName n)
+  Special _ sc -> Special () (uSpecialCon sc)
+
+uSpecialCon :: SpecialCon l -> SpecialCon ()
+uSpecialCon sc = case sc of
+  UnitCon _ -> UnitCon ()
+  ListCon _ -> ListCon ()
+  FunCon _ -> FunCon ()
+  TupleCon _ b i -> TupleCon () b i
+  Cons _ -> Cons ()
+  UnboxedSingleCon _ -> UnboxedSingleCon ()
+  ExprHole _ -> ExprHole ()
+  
+  
+uModuleName :: ModuleName l -> ModuleName ()
+uModuleName mn = case mn of
+  ModuleName _ n -> ModuleName () n
+
 uDeclHead :: DeclHead l -> DeclHead ()
 uDeclHead dhs = case dhs of
   DHead _ n -> DHead () $ uName n
@@ -44,7 +92,6 @@ uDeclHead dhs = case dhs of
   DHParen _ dh -> DHParen () (uDeclHead dh)
   DHApp _ dh t -> DHApp () (uDeclHead dh) (uTyVarBind t)
   
-
 uTyVarBind :: TyVarBind l -> TyVarBind ()
 uTyVarBind t = case t of
   KindedVar _ n k -> KindedVar () (uName n) (uKind k)
@@ -54,6 +101,8 @@ uName :: Name l -> Name ()
 uName n = case n of
   Ident _ s -> Ident () s
   Symbol _ s -> Symbol () s
+
+
 
 uKind :: Kind l -> Kind ()
 uKind ks = case ks of
