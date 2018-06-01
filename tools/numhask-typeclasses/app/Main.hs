e41c61abe5882e63546016593c4a2baed945a500
Marco Zocca <marco.zocca@recordunion.com>
2018-06-01 12:47:42 +0200

    scrap allll the boilerplate

--- tools/numhask-typeclasses/app/Main.hs
+++ tools/numhask-typeclasses/app/Main.hs
@@ -1,6 +1,7 @@
 {-# language OverloadedStrings #-}
 module Main where
 
+import Data.Maybe
 
 import Options.Applicative
 import Data.Semigroup
@@ -13,128 +14,38 @@ main :: IO ()
 main = do
   (CLOptions fpath) <- execParser clOpts
   mods <- fromParseResult <$> parseFile fpath
-  print $ head $ unpack mods
+  -- print $ head $ unpack mods
+  -- pure $ const () <$> unpack mods
+  let modh = stripAnnotations mods
+  -- pure $ map (\(a,b,c) -> const () <$> a <*> b <*> c) mods'
+  -- pure mods'
+  print modh
 
--- unpack :: Module l -> [Decl l]
-unpack modd = concat $ unpackTyClDecl <$> unpackModuleDecls modd
+stripAnnotations :: Module b -> Maybe (Decl ())
+stripAnnotations mo = head $ filter isJust $ uTyClDecl `map` unpackModuleDecls mo
 
 unpackModuleDecls :: Module l -> [Decl l]
 unpackModuleDecls moddecl = case moddecl of
   Module _ _ _ _ decls -> decls
   _ -> []
 
--- | Horrible hack to extract typeclass declarations
--- unpackTyClDecl :: Decl l -> [(Decl l)]
-unpackTyClDecl decl = case decl of
-  (ClassDecl _ a b c d) -> [(a,b,c,d)]
-  _ -> []
-
-
-
--- | Strip source position annotations 
-
--- uClassDecl c = case c of
---   ClsDecl _ ()
-
-
-
-
-uContext :: Context l -> Context ()
-uContext c = case c of
-  CxSingle _ a -> CxSingle () $ uAsst a
-
-uAsst :: Asst l -> Asst ()
-uAsst a = case a of
-  ClassA _ qn tys -> ClassA () (uQName qn) (map uType tys)
-  AppA _ n tys -> AppA () (uName n) (map uType tys)
-  InfixA _ ty0 qn ty -> InfixA () (uType ty0) (uQName qn) (uType ty)
-  IParam _ ipn ty -> IParam () (uIPName ipn) (uType ty)
-  EqualP _ ty ty1 -> EqualP () (uType ty) (uType ty1)
-  ParenA _ asst -> ParenA () (uAsst asst)
-  -- WildCardA _ mn ->
-
-uIPName :: IPName l -> IPName ()
-uIPName i = case i of
-  IPDup _ s -> IPDup () s
-  IPLin _ s -> IPLin () s
-
-uType :: Type l -> Type ()
-uType t = case t of
-  -- TyForall _
-  TyFun _ ty ty1 -> TyFun () (uType ty) (uType ty1)
-  TyTuple _ b tys -> TyTuple () b (map uType tys)
-
-uQName :: QName l -> QName ()
-uQName qn = case qn of
-  Qual _ mn n -> Qual () (uModuleName mn) (uName n)
-  UnQual _ n -> UnQual () (uName n)
-  Special _ sc -> Special () (uSpecialCon sc)
-
-uSpecialCon :: SpecialCon l -> SpecialCon ()
-uSpecialCon sc = case sc of
-  UnitCon _ -> UnitCon ()
-  ListCon _ -> ListCon ()
-  FunCon _ -> FunCon ()
-  TupleCon _ b i -> TupleCon () b i
-  Cons _ -> Cons ()
-  UnboxedSingleCon _ -> UnboxedSingleCon ()
-  ExprHole _ -> ExprHole ()
+-- | Strip source position annotations
+uTyClDecl :: Decl b -> Maybe (Decl ())
+uTyClDecl d = case d of
+  cd@ClassDecl{} -> Just $ const () <$> cd
+  _ -> Nothing
   
-  
-uModuleName :: ModuleName l -> ModuleName ()
-uModuleName mn = case mn of
-  ModuleName _ n -> ModuleName () n
-
-uDeclHead :: DeclHead l -> DeclHead ()
-uDeclHead dhs = case dhs of
-  DHead _ n -> DHead () $ uName n
-  DHInfix _ tyvb n -> DHInfix () (uTyVarBind tyvb) (uName n)
-  DHParen _ dh -> DHParen () (uDeclHead dh)
-  DHApp _ dh t -> DHApp () (uDeclHead dh) (uTyVarBind t)
-  
-uTyVarBind :: TyVarBind l -> TyVarBind ()
-uTyVarBind t = case t of
-  KindedVar _ n k -> KindedVar () (uName n) (uKind k)
-  UnkindedVar _ n -> UnkindedVar () (uName n)
-
-uName :: Name l -> Name ()
-uName n = case n of
-  Ident _ s -> Ident () s
-  Symbol _ s -> Symbol () s
-
-
-
-uKind :: Kind l -> Kind ()
-uKind ks = case ks of
-  KindStar _ -> KindStar ()
-  KindFn _ k1 k2 -> KindFn () (uKind k1) (uKind k2)
-  KindParen _ k -> KindParen () (uKind k)
-  -- KindVar _          -- qualified names
-  KindApp _ k1 k2 -> KindApp () (uKind k1) (uKind k2)
-  KindTuple _ kz -> KindTuple () $ map uKind kz
-  KindList _ k -> KindList () (uKind k)
-
-
-
-
-
-
-
 
 
 
 
 -- * Command line option parsing
 
-
 clOpts :: ParserInfo CLOptions
 clOpts = info (clOptions <**> helper) (
   fullDesc <> progDesc "Analyze a NumHask module with haskell-src-exts"
     -- <> header ""
                                       )
-
-
-
 
 data CLOptions = CLOptions {
   cloFilepath :: FilePath -- ^ file path of module to be analyzed
