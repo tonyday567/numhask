595c888e01cd893cac8d111657dd13797116361f
LeanderK <Leander.Kurscheidt@gmx.de>
2018-06-15 12:59:20 +0200

    added Homomorphism

---
+++ numhask/src/NumHask/Algebra/Abstract/Homomorphism.hs
@@ -0,0 +1,40 @@
+{-# OPTIONS_GHC -Wall #-}
+-- {-# LANGUAGE NoImplicitPrelude #-}
+-- {-# LANGUAGE UndecidableInstances #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE ConstrainedClassMethods #-}
+-- {-# LANGUAGE RoleAnnotations #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
+-- {-# LANGUAGE MonoLocalBinds #-}
+-- | The Homomorphism Hirarchy
+module NumHask.Algebra.Abstract.Homomorphism
+      ( 
+          Hom(..)
+      ,   Endo(..)
+      ,   Iso(..)
+      ,   Automorphism(..)
+      )
+      where
+
+import qualified Prelude                       as P
+import NumHask.Algebra.Abstract.Group
+
+-- | A Homomorphism between two magmas
+-- law: forall a b. hom(a `comb` b) = hom(a) `comb` hom(b)
+class (Magma a, Magma b) => Hom a b where
+    hom :: a -> b
+
+class (Hom a a) => Endo a
+instance (Hom a a) => Endo a
+
+-- | A Isomorphism between two magmas
+-- an Isomorphism is a bijective Homomorphism
+class (Hom a b, Hom b a) => Iso a b where
+    iso :: a -> b
+    iso = hom
+    invIso :: b -> a
+    invIso = hom
+
+class (Iso a a) => Automorphism a
+instance (Iso a a) => Automorphism a
