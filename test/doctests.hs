405d96dfd85f24b3a204a35ae5547866caab08eb
Tony Day <tonyday567@gmail.com>
2026-07-26 13:48:46 +1000

    Remove stale test/doctests.hs shim

--- test/doctests.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import System.Environment (getArgs)
-import Test.DocTest (mainFromCabal)
-import Prelude (IO, (=<<))
-
-main :: IO ()
-main = mainFromCabal "numhask" =<< getArgs
